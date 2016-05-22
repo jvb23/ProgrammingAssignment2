a918d15bd0be854d00ca137e1bd2c8d52f050ff1
Yuriy Sirtsov <jvbforjob@gmail.com>
2016-05-22 21:53:55 +0300

    First commit when assignment is done

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## this function creates an object with own functions(in some languages they are called
+##  methods) and data
 
 makeCacheMatrix <- function(x = matrix()) {
+  im <<- NULL
+  set<-function(y){
+    m <<- y
+    im <<- NULL
+  }
+  get<-function() m
+  setinv<-function(invm){
+    im <<- invm
+  }
+  getinv<-function() im
+  
+  list(getm = get,setm = set,seti = setinv,geti = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## this function is checking x object  wether it has cached inverse matrix or not yet.
+## And if yes then it returns cached(stored)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invm <-x$geti()
+  if (!is.null(invm)){
+    message("Getting cached data")
+    return(invm)
+  }
+      
+  matr <- x$getm()
+  invm <-solve(matr)
+  x$seti(invm)
+  invm
 }
